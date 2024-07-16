e64aee68cc879e7fe3e3b0f3db4fbafbc6572fd4
Kowshik Deb Nath <48962097+kowshik24@users.noreply.github.com>
2024-07-17 00:56:33 +0600

    Updated the host on app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ async def prediction(
     return JSONResponse(content={"prediction": float(prediction)})
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9696)
+    uvicorn.run(app, port=9696)
